d378091d4c6877e009bcd4531470c72e016172cb
Sandro Matias <devsandromatias@gmail.com>
2024-04-10 23:07:24 -0300

    programming logica search

--- src/shared/Components/Main/Sections/SectionOne/SearchSection.tsx
+++ src/shared/Components/Main/Sections/SectionOne/SearchSection.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { CMain, CSearch, RSearch } from "./SearchSection.styles";
 
 const SearchSection = () =>{
+
+  const [ search, setSearch ] = useState("")
+
+  console.log(search)
+
   return (
     <CMain>
       <CSearch>
         <h2>Faça Aqui sua Pesquisa</h2>
         <p>Arquitetura e Organização de Computadores e Sistemas Operacionais</p>
-        <input type="search" placeholder="Faça sua Pesquisa..."/>
+        <input 
+        type="search" 
+        placeholder="Faça sua Pesquisa..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
         <button type="submit">
           Buscar
         </button>
