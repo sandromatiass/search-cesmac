b529d26b9866fb5a387c4102ad02c44d8fc60ada
Sandro Matias <devsandromatias@gmail.com>
2024-04-16 22:33:28 -0300

    implement logic search

--- src/shared/Components/Main/Sections/SectionOne/SearchSection.tsx
+++ src/shared/Components/Main/Sections/SectionOne/SearchSection.tsx
@@ -1,10 +1,40 @@
 import { useState } from "react";
 import { CMain, CSearch, RSearch } from "./SearchSection.styles";
+import dataContent from "./data/data.json"
+
+interface SubThemes {
+  title: string;
+  content: string;
+}
 
 const SearchSection = () =>{
 
-  const [ search, setSearch ] = useState("")
+  const [ search, setSearch ] = useState<string>("")
+  const [ searchResult,  setSearchResults ] =  useState<SubThemes[]>([]);
 
+  const handleSearch = () => {
+    if (!search.trim()) {
+      setSearchResults([]);
+      return;
+    }
+
+    const keywords = search.toLowerCase().split(" ");
+    const results = dataContent.subthemes.filter((item) => {
+      const title = item.title ? item.title.toLowerCase() : "";
+      const content = item.content.toLowerCase();
+      return keywords.some(keyword => title.includes(keyword) || content.includes(keyword));
+    });
+    setSearchResults(results.map(item => ({
+      title: item.title || item.nome || "",
+      content: item.content
+    })));
+  };
+
+  const clearSearch = () => {
+    setSearch("");
+    setSearchResults([]);
+  }
+  
   console.log(search)
 
   return (
@@ -18,13 +48,21 @@ const SearchSection = () =>{
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         />
-        <button type="submit">
+        <button type="button" onClick={handleSearch}>
           Buscar
+        </button>
+        <button type="button" onClick={clearSearch}>
+          Limpar Pesquisa
         </button>
       </CSearch>
 
       <RSearch>
-        
+        {searchResult.map((item, index) => (
+          <div key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.content}</p>
+          </div>
+        ))}
       </RSearch>
     </CMain>
   )
